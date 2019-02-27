aed3c980f0c9f1f8990c62062d100e1512136db3
fdumani <fabrizio_d_b@hotmail.com>
2019-02-27 17:05:12 -0600

    Added saxpy algorithm wiht both secuencial and parallel methods

---
+++ saxpy_par.c
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////
+// Fabrizio Dumani Bernardi
+// Carné: 2015182277
+// Arquitectura de computadores II
+// Taller 2: OpenMP
+// Algoritmo SAXPY con paralelizacion implementada
+///////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define array_size 1000000 // Size for arrays to be used
+#define a 10 // Constant for SAXPY algorithm
+
+int main ()
+{
+	double start_time, run_time;
+	int x[array_size];
+	int y[array_size];
+	
+	int i;
+	// The next loop fills arrays "x" and "y" with random values
+	for (i=0; i < array_size; i++) {
+		x[i] = rand() % 255; //Create a random number between 0 and 255
+		y[i] = rand() % 255;
+	}
+	
+	int j;
+	
+	start_time = omp_get_wtime();
+	
+	// Define the paralel region
+	#pragma omp parallel  
+	{
+		// Paralel threads created to improve the for loop
+		#pragma omp for
+		//The next loop executes SAXPY algorithm between "x" and "y" using "a" as the constant for multiplication
+		for (j=0; j < array_size; j++) {
+			
+			//printf("%d + 10*%d = ", y[j], x[j]);
+			y[j] = a*x[j] + y[j]; // SAXPY function
+			//printf("%d\n", y[j]);
+			
+		}
+	}
+	run_time = omp_get_wtime() - start_time;
+	printf("Run time = %f\n", run_time);
+}
